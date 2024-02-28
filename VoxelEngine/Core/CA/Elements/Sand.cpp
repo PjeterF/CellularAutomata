07c9de97afe6826f6cb5c6dd07b429245940d249
Piotr Kupniewski <kupik59@gmail.com>
2024-02-28 11:08:29 +0100

    a

--- VoxelEngine/Core/CA/Elements/Sand.cpp
+++ VoxelEngine/Core/CA/Elements/Sand.cpp
@@ -3,6 +3,7 @@
 #include "Vaccum.hpp"
 #include "../ImmovableSolid.hpp"
 #include "../MovableSolid.hpp"
+#include <algorithm>
 
 Sand::Sand(glm::vec4 color) : MovableSolid(color)
 {
@@ -11,37 +12,57 @@ Sand::Sand(glm::vec4 color) : MovableSolid(color)
 
 void Sand::update(glm::vec2 position, CellGrid& grid)
 {
-	Cell* target;
+	static float velIncrease = 0.5f;
+	static float maxVel = 5.0f;
+	static float frictionScale = 0.5f;
+	Cell* target = nullptr;
+	glm::ivec2 targetPos = position;
 
 	for (int i = 0; i < this->downwardsVelocity; i++)
 	{
 		target = grid.getCell(position.x, position.y - 1 - i);
+		targetPos = { position.x, position.y - 1 - i };
+		if (target == nullptr)
+		{
+			target = grid.getCell(position.x, position.y - i);
+			targetPos = { position.x, position.y - i };
+			break;
+		}
+		if (!(target->type == Empty || target->type == Gas || target->type == Liquid))
+		{
+			target = grid.getCell(position.x, position.y - i);
+			targetPos = { position.x, position.y - i };
+			break;
+		}
 	}
 
-	target = grid.getCell(position.x, position.y - 1);
-	if (target == nullptr)
-		return;
+	this->downwardsVelocity = std::min(this->downwardsVelocity + velIncrease, maxVel);
+
 	if (target->type == Empty || target->type == Gas || target->type == Liquid)
 	{
-		grid.swapCells(position, { position.x, position.y - 1 });
+		grid.swapCells(position, targetPos);
 		return;
 	}
 
 	target = grid.getCell(position.x + 1, position.y - 1);
+	targetPos = { position.x + 1 + this->downwardsVelocity/2 , position.y - 1 };
 	if (target == nullptr)
 		return;
 	if (target->type == Empty || target->type == Gas || target->type == Liquid)
 	{
-		grid.swapCells(position, { position.x + 1, position.y - 1 });
+		this->downwardsVelocity = frictionScale * this->downwardsVelocity;
+		grid.swapCells(position, targetPos);
 		return;
 	}
 
 	target = grid.getCell(position.x - 1, position.y - 1);
+	targetPos = { position.x - 1 - this->downwardsVelocity / 2 , position.y - 1 };
 	if (target == nullptr)
 		return;
 	if (target->type == Empty || target->type == Gas || target->type == Liquid)
 	{
-		grid.swapCells(position, { position.x - 1, position.y - 1 });
+		this->downwardsVelocity = frictionScale * this->downwardsVelocity / 10;
+		grid.swapCells(position, targetPos);
 		return;
 	}
 }
